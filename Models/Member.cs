30c5ed2307f3a91215a4059d7a62ff984331f020
samreichle <samjhreichle@gmail.com>
2020-11-21 10:14:49 -0600

    Created Member class model

---
+++ Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ua_acm_website.Models
+{
+    public class Member
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
